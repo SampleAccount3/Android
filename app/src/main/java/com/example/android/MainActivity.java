a00721ff8f4c8b577c73139ab0e68a054c1f9222
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-04-11 16:17:39 +0800

    Done with the UI

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.example.android.Model.CustomerModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnViewAll,btnAdd;
+    EditText etName,etAge;
+    CheckBox isActive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnViewAll = findViewById(R.id.btn_ViewAll);
+        btnAdd = findViewById(R.id.btn_Add);
+        etName = findViewById(R.id.et_Name);
+        etAge = findViewById(R.id.et_Age);
+        isActive = findViewById(R.id.cb_Customer);
+
+        btnViewAll.setOnClickListener((view)->{
+            Toast.makeText(this, "Hello World View All", Toast.LENGTH_SHORT).show();
+
+            CustomerModel customerModel = new CustomerModel(-1,etName.getText().toString(),
+                    Integer.parseInt(etAge.getText().toString()),isActive.isChecked());
+        });
+
+        btnAdd.setOnClickListener((view)->{
+            Toast.makeText(this, "Hello World Add", Toast.LENGTH_SHORT).show();
+        });
     }
 }
---
+++ app/src/main/java/com/example/android/Model/CustomerModel.java
@@ -0,0 +1,25 @@
+package com.example.android.Model;
+
+public class CustomerModel {
+    private int id;
+    private String name;
+    private int age;
+    private boolean isActiveCustomer;
+
+    public CustomerModel(int id, String name, int age, boolean isActiveCustomer) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.isActiveCustomer = isActiveCustomer;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", isActiveCustomer=" + isActiveCustomer +
+                '}';
+    }
+}
